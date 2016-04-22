1d1736583b03fafb24fa5e94de0f138773a392df
Ken Sia <kennethpaulosia@yahoo.com>
2016-04-22 14:37:01 +0800

    commit 4 22 2016
    files modified:
    app.js
    directoryController.js
    directoryView,html
    forgotPasswordController.js
    loginView.html
    registerView.html
    index.html
    
    added files:
    memberProfileController.js
    memberProfileView.html
    myProfileController.js
    myProfileView.html

---
+++ src/app/components/profile/memberProfileController.js
@@ -0,0 +1,58 @@
+(function() {
+
+	angular
+		.module('entreprenityApp.memberProfile', [])
+
+		.factory('memberProfileService', function($http) {
+		
+			var baseUrl = 'api/';
+			return {
+				getMemberProfile: function(id) {
+					return $http.get(url, id);
+				}
+			};
+		})
+
+		.controller('memberProfileController', function($routeParams, memberProfileService) {
+			var vm = this;
+			vm.memberId = $routeParams.memberId;
+			
+
+			data = {
+				"id": 1,
+				"avatar": "member01.jpg",
+				"coverPhoto": "memberCover01.jpg",
+				"firstName": "Ken",
+				"lastName": "Sia",
+				"position": "Developer",
+				"city": "Taguig",
+				"followers": "2",
+				"following": "10",
+				"aboutMe": "Freelance WEb Developer who loves listening to music, surfing, and traveling",
+				"email": "ken.voffice@gmail.com",
+				"website": "ken.com.ph",
+				"mobile": "09175296299",
+				"company": {
+					"companyName": "voffice",
+					"companyDesc": "We provide businesses superior reach and access to South East Asia markets like Jakarta, Manila, Kuala Lumpur and Singapore"
+				},
+				"skills": [
+					{"item": "Programming"},
+					{"item": "Public Speaking"}
+				],
+				"interests": [
+					{"item": "Design"},
+					{"item": "Surf"},
+					{"item": "Basketball"}
+				]
+			};
+
+		
+			vm.member = data;
+			/*
+			memberProfileService.getMemberProfile(vm.memberId).success(function(data) {
+				vm.member = data;
+			});	
+			*/
+		});			
+})();
