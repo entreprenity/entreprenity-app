b35904fa8e017779ea675fb5c4e6099db79a8e8e
Ken Sia <kennethpaulosia@yahoo.com>
2016-04-27 20:06:43 +0800

    commit 4 27 2016 part 3
    commit 4 27 2016 part 3

--- src/app/components/profile/companyProfileController.js
+++ src/app/components/profile/companyProfileController.js
@@ -8,54 +8,54 @@
 		var baseUrl = 'api/';
 		return {
 			getCompanyProfile: function(id) {
-				return $http.get(baseUrl+ 'view_user_profile?id='+id);
+				return $http.get(baseUrl+ 'view_company_profile?id='+id);
 			}
 		};
 	})
 
 		.controller('companyProfileController', function($routeParams, companyProfileService) {
 		var vm = this;
-		vm.companyId = $routeParams.memberId;
-
-		data = {
-			"id": 1,
-			"name": "vOffice",
-			"location": "Fort Legend Tower",
-			"coverPhoto": "cover.jpg",
-			"profilePhoto": "company-default.jpg",
-			"website": "voffice.com.ph",
-			"email": "sales@voffice.com",
-			"mobile": "639175296299",
-			"tel": "6322931533",
-			"fax": "6329165745",
-			"desc": "We provide businesses superior reach and access to South East Asia markets like Jakarta, Manila, Kuala Lumpur and Singapore.",
-			"followers": "10",
-			"categories": [
-				"virtual office",
-				"serviced office",
-				"co-working spaces"
-			],
-			"employees": [
-				{
-					"id": "1",
-					"firstName": "Ken",
-					"lastName": "Sia",
-					"profilePhoto": "member01.jpg"
-				},
-				{
-					"id": "2",
-					"firstName": "Jaye",
-					"lastName": "Atienza",
-					"profilePhoto": "member02.jpg"
-				}
-			]
-		};
-
-			vm.company = data;
-			
-			/*
-		  memberProfileService.getCompanyProfile(vm.memberId).success(function(data) {
-			vm.member = data;
+		vm.companyId = $routeParams.companyId;
+		/*
+				data = {
+					"id": 1,
+					"name": "vOffice",
+					"location": "Fort Legend Tower",
+					"coverPhoto": "cover.jpg",
+					"profilePhoto": "company-default.jpg",
+					"website": "voffice.com.ph",
+					"email": "sales@voffice.com",
+					"mobile": "639175296299",
+					"tel": "6322931533",
+					"fax": "6329165745",
+					"desc": "We provide businesses superior reach and access to South East Asia markets like Jakarta, Manila, Kuala Lumpur and Singapore.",
+					"followers": "10",
+					"categories": [
+						"virtual office",
+						"serviced office",
+						"co-working spaces"
+					],
+					"employees": [
+						{
+							"id": "1",
+							"firstName": "Ken",
+							"lastName": "Sia",
+							"profilePhoto": "member01.jpg"
+						},
+						{
+							"id": "2",
+							"firstName": "Jaye",
+							"lastName": "Atienza",
+							"profilePhoto": "member02.jpg"
+						}
+					]
+				};
+				vm.company = data;
 			*/
-		});			
+
+		companyProfileService.getCompanyProfile(vm.companyId).success(function(data) {
+			vm.company = data;
+		});
+
+	});			
 })();
