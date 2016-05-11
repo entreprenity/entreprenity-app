3df628f3ef4abbab70b692f8ae716b4fcc3e2204
Ken Sia <kennethpaulosia@yahoo.com>
2016-05-11 18:34:22 +0800

    commit 05 11 2016
    - join function in events module
    - latestevents module
    - _home style

--- src/app/components/events/eventsPageController.js
+++ src/app/components/events/eventsPageController.js
@@ -10,6 +10,8 @@
 			getEventProfile: function(id) {
 				return $http.get(baseUrl+ 'view_event_detail?id='+id);
 			}
+			//post service for joinEvent()
+			//post service for unJoinEvent()
 		};
 	})
 
@@ -17,9 +19,36 @@
 		var vm = this;
 
 		vm.eventId = $routeParams.eventId;
+		
 
 		eventsPageService.getEventProfile(vm.eventId).success(function(data) {
-		vm.event = data;
+			vm.event = data;
+			vm.event.joining = false;
 		});
+		
+		vm.joinEvent = function() {
+			//post service
+			var attendees = [{
+				"id": 1,
+				"avatar": "member01.jpg",
+				"coverPhoto": "memberCover01.jpg",
+				"firstName": "Ken",
+				"lastName": "Sia",
+				"position": "Front-end Web Developer",
+				"city": "Taguig",
+			}];
+			
+			vm.event.joining = true;
+			vm.event.attendees = attendees;
+		}
+		
+		
+		vm.unJoinEvent = function() {
+			//post service
+			var attendees = [];
+
+			vm.event.joining = false;
+			vm.event.attendees = attendees;
+		}
 	});			
 })();
