e495312442a6cb1d979906e176856fd23d4e1a4a
Ken Sia <kennethpaulosia@yahoo.com>
2016-08-18 18:07:22 +0800

    view and controller edit on notif

--- src/app/common/headerController.js
+++ src/app/common/headerController.js
@@ -51,8 +51,9 @@
 					}
 					AuthService.logOut(token);
 				}
-				
-				vm.readNotifications = function() {
+					
+				$scope.numOfNotifications = 5;// need a service that will store the number of unred notif to this variable
+				$scope.readNotifications = function() {
 					//service to update notif read number to 0
 					//add ng-click="vm.readNotifications()"
 				}
