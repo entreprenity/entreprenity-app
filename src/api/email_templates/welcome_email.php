f84d0ae8f32608a1954ec22a81e5e549afe5c010
Ken Sia <kennethpaulosia@yahoo.com>
2016-08-26 09:59:07 +0800

    welcome email redesign

---
+++ src/api/email_templates/welcome_email.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
+	<meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
+	<meta name="format-detection" content="telephone=no">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="format-detection" content="date=no">
+	<meta name="format-detection" content="address=no">
+	<meta name="format-detection" content="email=no">
+	<title>Welcome to Entreprenity</title>
+	<style type="text/css">
+		body,table.body,h1,h2,h3,h4,h5,h6,p,td {
+			font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;
+			font-size: 14px;
+			color: #202020;
+			line-height: 1.4;
+		}
+		h1, p {
+			padding-top: 15px;
+			padding-bottom: 15px;
+			margin-top: 0px;
+			margin-bottom: 0px;
+		}
+
+		@media only screen and (max-width: 450px) {
+			td[class="drop-column"] {
+				display: block;
+				width: 100%;
+				-moz-box-sizing: border-box;
+				-webkit-box-sizing: border-box;
+				box-sizing: border-box;
+			}
+		}
+	</style>
+	<style type="text/css">
+		table {border-collapse:separate;}
+		a, a:link, a:visited {text-decoration: none; color: #00788a}
+		a:hover {text-decoration: underline;}
+		h2,h2 a,h2 a:visited,h3,h3 a,h3 a:visited,h4,h5,h6,.t_cht {color:#000 !important}
+		.ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td {line-height: 100%}
+		.ExternalClass {width: 100%;}
+	</style>
+</head>
+<body style="background-color:#EEEEEE;padding-top:10px;padding-bottom:10px;padding-right:10px;padding-left:10px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+<span style="display:none !important;" >
+      Welcome to Entreprenity
+      </span>
+<table border="0" cellpadding="0" cellspacing="0"  width="100%" style="margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;border-collapse:separate;" >
+	<tr>
+		<td align="center" valign="top" style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+			<table cellpadding="0" cellspacing="0" style="border-collapse:separate;" >
+				<tr>
+					<td class="pattern" width="580" style="background-color:#ffffff;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+						<table cellpadding="0" cellspacing="0" style="border-collapse:separate;" >
+							<tr>
+								<td class="header" style="height:56px;background-color:#222222;width:580px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<table cellpadding="0" cellspacing="0" style="width:100%;border-collapse:separate;" >
+										<tr>
+											<td align="center" style="width:100%;padding-left:10px;padding-right:10px;padding-top:10px;padding-bottom:10px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+												<img src="http://entreprenity.co/app/assets/img/entreprenity-logo.png" alt="" style="width:200px;display:block;border-width:0;" />
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td style="height:56px;width:580px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<table cellpadding="0" cellspacing="0" style="width:100%;border-collapse:separate;" >
+										<tr>
+											<td align="center" style="width:100%;padding-left:10px;padding-right:10px;padding-top:20px;padding-bottom:20px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+												<img src="http://entreprenity.co/app/assets/img/entrp-welcome-banner.png" alt="" style="display:block;border-width:0;" />
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" style="padding-top:10px;padding-left:15%;padding-right:15%;text-align:center;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<table cellpadding="0" cellspacing="0" style="border-collapse:separate;" >
+										<tr>
+											<td align="center" style="padding-left:10px;padding-right:10px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+												<h1 style="font-size:32px;font-weight:bold;border-bottom-style:dotted;border-bottom-color:#f29b13;border-bottom-width:2px;padding-bottom:0;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;color:#202020;line-height:1.4;padding-top:15px;margin-top:0px;margin-bottom:0px;" ><?php echo (isset($userName) && $userName != '' ? '@'.$userName : ''); ?></h1>
+												<p style="padding-top:30px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;padding-bottom:15px;margin-top:0px;margin-bottom:0px;" >Your Entreprinity account is now Activated! Please find the login info below.</p>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" style="height:50px;padding-left:15%;padding-right:15%;padding-bottom:20px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<div style="padding-top:10px;padding-bottom:10px;padding-left:10px;padding-right:10px;border-color:#f29b13;border-width:1px;border-style:solid;cursor:pointer;" >
+										<p style="color:inherit;text-decoration:none;padding-bottom:3px;padding-top:3px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;line-height:1.4;margin-top:0px;margin-bottom:0px;" ><strong>WEB APP URL :</strong> http://entreprenity.co/app</p>
+										<p style="color:inherit;text-decoration:none;padding-bottom:3px;padding-top:3px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;line-height:1.4;margin-top:0px;margin-bottom:0px;" ><strong>YOUR USERNAME :</strong> <?php echo (isset($userEmail) && $userEmail != '' ? $userEmail : ''); ?> </p>
+										<p style="color:inherit;text-decoration:none;padding-bottom:3px;padding-top:3px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;line-height:1.4;margin-top:0px;margin-bottom:0px;" ><strong>YOUR PASSWORD :</strong> <?php echo (isset($userPass) && $userPass != '' ? $userPass : ''); ?> </p>
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" style="padding-left:10px;padding-right:10px;padding-bottom:20px;padding-top:20px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<a href="http://entreprenity.co/app" style="text-decoration:none;background-color:#000000;color:#ffffff;font-size:18px;padding-left:20px;padding-right:20px;padding-top:15px;padding-bottom:15px;" ><strong>CLICK HERE TO LOGIN</strong></a>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" style="padding-top:10px;padding-left:15%;padding-right:15%;text-align:center;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<table cellpadding="0" cellspacing="0" style="border-collapse:separate;" >
+										<tr>
+											<td align="center" style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+												<div>
+													<p style="padding-top:10px;padding-bottom:10px;padding-right:10px;padding-left:10px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;margin-top:0px;margin-bottom:0px;" >Login today to immediately connect with other business owners via the main Time Line section.</p>
+													<p style="padding-top:10px;padding-bottom:10px;padding-right:10px;padding-left:10px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;margin-top:0px;margin-bottom:0px;" >Seeking a graphic designer for your latest brochure? Or perhaps you are a web developer who can provide services to other Entreprenity members? Post it on the Business Opportunities section to get automatic Service Seeker and Provider matching.</p>
+													<p style="padding-top:10px;padding-bottom:10px;padding-right:10px;padding-left:10px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;margin-top:0px;margin-bottom:0px;" >Post it on the Business Opportunities section to get automatic Service Seeker and Provider matching</p>
+												</div>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" style="padding-top:10px;padding-left:15%;padding-right:15%;text-align:center;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<p style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;padding-top:15px;padding-bottom:15px;margin-top:0px;margin-bottom:0px;" ><strong style="color:#f29b13;font-size:18px;" >Entreprenity</strong> <strong>can also</strong></p>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" style="padding-top:10px;padding-left:15%;padding-right:15%;text-align:center;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<table width="100%" cellpadding="0" cellspacing="0" style="border-collapse:separate;" >
+										<tr>
+											<td class="drop-column" width="50%" align="center" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+												<img src="http://entreprenity.co/app/assets/img/entrp-welcome-notes.png" alt="" style="width:80px;display:block;border-width:0;" />
+												<p style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;padding-top:15px;padding-bottom:15px;margin-top:0px;margin-bottom:0px;" >Let you book a Meeting Room & Work Space</p>
+											</td>
+											<td class="drop-column" width="50%" align="center" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+												<img src="http://entreprenity.co/app/assets/img/entrp-welcome-call.png" alt="" style="width:80px;display:block;border-width:0;" />
+												<p style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;padding-top:15px;padding-bottom:15px;margin-top:0px;margin-bottom:0px;" >Let you book a Meeting Room & Work Space</p>
+											</td>
+										</tr>
+										<tr>
+											<td class="drop-column" width="50%" align="center" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+												<img src="http://entreprenity.co/app/assets/img/entrp-welcome-calendar.png" alt="" style="width:80px;display:block;border-width:0;" />
+												<p style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;padding-top:15px;padding-bottom:15px;margin-top:0px;margin-bottom:0px;" >Let you book a Meeting Room & Work Space</p>
+											</td>
+											<td class="drop-column" width="50%" align="center" style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+												<img src="http://entreprenity.co/app/assets/img/entrp-welcome-key.png" alt="" style="width:80px;display:block;border-width:0;" />
+												<p style="font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;padding-top:15px;padding-bottom:15px;margin-top:0px;margin-bottom:0px;" >Let you book a Meeting Room & Work Space</p>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" style="padding-top:10px;padding-left:15%;padding-right:15%;text-align:center;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<p style="font-size:18px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;color:#202020;line-height:1.4;padding-top:15px;padding-bottom:15px;margin-top:0px;margin-bottom:0px;" ><strong>and many more features awaiting for you to discover!</strong></p>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" style="padding-left:10px;padding-right:10px;padding-bottom:40px;padding-top:20px;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+									<a href="http://entreprenity.co" style="text-decoration:none;background-color:#f29b13;color:#ffffff;font-size:18px;padding-left:20px;padding-right:20px;padding-top:15px;padding-bottom:15px;" ><strong>GO TO WEBSITE</strong></a>
+								</td>
+							</tr>
+							<tr>
+								<td class="footer" align="center" valign="center" style="height:56px;border-top-color:#C9CBCD;border-top-width:1px;border-top-style:solid;padding-left:15%;padding-right:15%;padding-bottom:2%;padding-top:2%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;font-size:14px;color:#202020;line-height:1.4;" >
+                                 <span style="color:#C9CBCD;font-size:12px;" >Entreprenity is currently in BETA phase. So please, do let us know what issues you are facing while using the app. Do feed back to us at <a style="text-decoration:none;color:#00788a;" >feedback@entreprininty.co</a>
+                                 when you found one.</span>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
